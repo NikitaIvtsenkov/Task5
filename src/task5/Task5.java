c4b91a5b7f2a53c0f40276561c23cc80a0ec6c18
Nikita <102020471+NikitaIvtsenkov@users.noreply.github.com>
2022-09-28 15:02:33 +0300

    Update Task5.java

--- src/task5/Task5.java
+++ src/task5/Task5.java
@@ -1,21 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package task5;
 
-/**
- *
- * @author pupil
- */
-public class Task5 {
+import java.util.Random;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class task5 {
+    static int getIntFromRange(int min, int max) {
+            Random random = new Random();
+            return random.nextInt(max - min + 1) + min;
     }
-    
+    public static void main(String[] args){
+        int[][] mat = new int[5][];
+        mat[0] = new int[5];
+        mat[1] = new int[4];
+        mat[2] = new int[3];
+        mat[3] = new int[4];
+        mat[4] = new int[5];
+        for (int[] matrixlenght : mat) {
+            for (int j = 0; j < matrixlenght.length; j++) {
+                matrixlenght[j] = getIntFromRange(50, 100);
+                System.out.printf("%5d", matrixlenght[j]);
+            }
+            System.out.println();
+        }
+    System.out.println("---------------------");
+        for (int[] mat1 : mat) {
+            for (int j = 0; j < mat1.length; j++) {
+                for (int[] mat2 : mat) {
+                    for (int m = 0; m < mat2.length; m++) {
+//                        if (mat1[j] < mat2[m]) {
+//                            mat1[j] = mat1[j] + mat2[m];
+//                            mat2[m] = mat1[j] - mat2[m];
+//                            mat1[j] = mat1[j] - mat2[m];
+//                        } 
+                        if (mat1[j] > mat2[m]) {
+                            mat1[j] = mat2[m];
+                            
+                            
+                        } 
+                    }
+                }
+            }
+        }
+        for (int[] matrixlenght : mat) {
+            for (int j = 0; j < matrixlenght.length; j++) {
+                System.out.printf("%5d", matrixlenght[j]);   
+            }
+            System.out.println();    
+        }
+    }
 }
